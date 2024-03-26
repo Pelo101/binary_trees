a963febe69cee6640e07709147b2a8205ab48d20
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-03-26 10:13:27 -0700

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -33,7 +33,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		newNode->left = parent->left;
-		newNode->left->parent = newNode;;
+		newNode->left->parent = newNode;
 	}
 	parent->left = newNode;
 
