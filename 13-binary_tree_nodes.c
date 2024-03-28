c0ea7f5699cb1899227800eb62cf5c4d53addb52
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-03-28 09:57:15 -0700

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -22,7 +22,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		nodes++;
 
-		:nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->left);
 		nodes +=  binary_tree_nodes(tree->right);
 	}
 		return (nodes);
